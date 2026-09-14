4d9b59bb020b633d069e1c8f89b3d9b5e001d645
Miguel Barroso <git@miguelbarroso.com>
2026-09-14 17:15:22 +0900

    packaging: pin the cask's sha256 to the v2.3.2 tarball

--- packaging/homebrew/warmline.rb
+++ packaging/homebrew/warmline.rb
@@ -1,6 +1,6 @@
 cask "warmline" do
-  version "2.3.1"
-  sha256 "f860e9f179c51b81da45d05ef5cf458ade5254c80b9645657cfa8ca25fa68dcb"
+  version "2.3.2"
+  sha256 "578f86647f15e303d057619d4b8365d76314fed1cd721b9a09d3db12eee07215"
 
   url "https://github.com/Miguel-Barroso/claude-warmline/archive/refs/tags/v#{version}.tar.gz"
   name "claude-warmline"
